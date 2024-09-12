bf85d24e505ada167054153e93f67c50cbf9116a
Fabian Speh <132446305+FabianSpeh@users.noreply.github.com>
2024-09-12 12:52:10 +0200

    added registration endpoint, login endpoint, database connection

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,44 @@ const express = require("express");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
-mongoose.connect(MONGOURL).then(() => {
-  console.log("Database successfully connected");
-});
-app.get("/", (req, res) => {
-  res.send("Hello");
+mongoose
+  .connect(MONGOURL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Database not connected", err));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/api/wiki/:title", async (req, res) => {
+  const title = req.params.title;
+  const url = "https://en.wikipedia.org/w/api.php";
+  const params = {
+    action: "parse",
+    page: title,
+    format: "json",
+    prop: "text",
+    disablelimitreport: "1",
+    disableeditsection: "1",
+    disabletoc: "1",
+  };
+  try {
+    const response = await axios.get(url, { params });
+    const htmlContent = response.data.parse.text["*"];
+    res.json({ html: htmlContent });
+  } catch (error) {
+    console.error("Error fetching Wikipedia page:", error);
+    res.status(500).send("Error fetching Wikipedia page");
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`runs on ${PORT}`);
-  console.log(MONGOURL);
-});
+app.use("/", require("./routes/authRoutes"));
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,28 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import axios from "axios";
+import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import { UserContextProvider } from "../context/userContext";
+import Wikipedia from "./pages/Wikipedia";
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
+      <Navbar />
+      <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/wiki" element={<Wikipedia />} />
       </Routes>
-    </>
+    </UserContextProvider>
   );
 }
 
